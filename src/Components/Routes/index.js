71c06152d9000a0181116e68caf07996166445a7
Fan Yiwei <nf140901@gmail.com>
2023-05-02 22:46:34 +0800

    add routes and logo for header

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router-dom";
+import { lazy } from "react";
 import Home from '../../Pages/Home'
 import Login from '../../Pages/Login'
 import Cart from '../../Pages/Cart'
