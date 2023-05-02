2d8cce74687cbcd634bdf8cec54497b3051fc112
Fan Yiwei <nf140901@gmail.com>
2023-05-02 23:06:17 +0800

    add footer and add sprite bgc

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './index.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <Fragment>
+      <div className="footer">
+        <div className="wrapper">
+          <div className="badges">
+            <p className="price">价格亲民</p>
+            <p className="express">物流快捷</p>
+            <p className="quality">品质新鲜</p>
+          </div>
+          <div className="copyright">
+            <p>
+              <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>About Us</a>
+              <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>Help Center</a>
+              <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>Our Services</a>
+              <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>Delievery</a>
+              <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>Business Corporation</a>
+            </p>
+            <p>CopyRight &copy; E-Shopper</p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   )
 }
