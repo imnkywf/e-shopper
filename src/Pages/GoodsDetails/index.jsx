cc507ac291b3446717718f8fbe3e7a880246b548
Fan Yiwei <nf140901@gmail.com>
2023-05-05 22:35:46 +0800

    add good detail components

--- src/Pages/GoodsDetails/index.jsx
+++ src/Pages/GoodsDetails/index.jsx
@@ -1,14 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import getData from '../../APIs/ajaxRequest'
+import './index.css'
 
 export default function GoodsDetails() {
+  const [data, setData] = useState([])
+  const [product, setProduct] = useState({})
+  const [quantity, setQuantity] = useState(1)
   const { id } = useParams()
-  return (
-    <div>
-      <ul>
-        <li>{id}</li>
 
-      </ul>
+  useEffect(() => {
+    const getProducts = async () => {
+      const p1 = await getData('http://localhost:5000/api/getnewgoods')
+      const p2 = await getData('http://localhost:5000/api/getproducts')
+      setData([...p1, ...p2])
+    };
+    getProducts()
+  }, [])
+
+  useEffect(() => {
+    const foundProduct = data.find(e => e.id === +id)
+    if (foundProduct) {
+      setProduct(foundProduct)
+    }
+  }, [data, id])
+
+  const handleChangeQuantity = (e) => {
+    switch (e.target.className) {
+      case 'plus-btn':
+        setQuantity(quantity >= 99 ? (prevQty) => prevQty : (prevQty) => prevQty + 1)
+        break
+      case 'minus-btn':
+        setQuantity(quantity <= 1 ? (prevQty) => prevQty : (prevQty) => prevQty - 1)
+        break
+      default:
+        break
+    }
+
+
+  }
+
+  const addToCart = () => {
+
+  }
+
+  return (
+    <div className="product-details">
+      <div className="wrapper">
+        <div className="product-image">
+          <img src={product.img_url} alt={product.name} />
+        </div>
+        <div className="product-info">
+          <h1>{product.name}</h1>
+          <h2>${product.price}</h2>
+          <p>{product.description}</p>
+          <div className="quantity">
+            <label htmlFor="quantity">Quantity:</label>
+            <button className="minus-btn" onClick={handleChangeQuantity}>-</button>
+            <input
+              type="text"
+              id="quantity"
+              name="quantity"
+              min="1"
+              value={quantity}
+              onChange={handleChangeQuantity}
+            />
+            <button className="plus-btn" onClick={handleChangeQuantity}>+</button>
+          </div>
+          <button onClick={addToCart}>Add to Cart</button>
+        </div>
+      </div>
     </div>
   )
 }
